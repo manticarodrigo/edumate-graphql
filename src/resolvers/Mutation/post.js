acbdafa1c213c9440cfadf0c06ae711616e41988
Rodrigo Mantica <manticarodrigo@gmail.com>
2018-03-14 13:18:01 -0600

    poll endDate and resolver refactor

--- src/resolvers/Mutation/post.js
+++ src/resolvers/Mutation/post.js
@@ -2,32 +2,24 @@ const { getUserId } = require('../../utils')
 const { processUpload } = require('../../modules/fileApi')
 
 const post = {
-  async createPost(parent, { text, files, pollOptions }, ctx, info) {
-    const userId = getUserId(ctx)
+  async createPost(parent, { text, files, pollOptions, pollEndDate }, ctx, info) {
+    const author = { connect: { id: getUserId(ctx) } }
     const images = files && files.length > 0 ? { create: await Promise.all(files.map(file => processUpload(file, ctx))) } : null
-    const poll = pollOptions && pollOptions.length > 0 ? { create: { options: { create: pollOptions.map(option => { return { name: option } }) } } } : null
+    const poll = pollOptions && pollOptions.length > 1 ? { create: { options: { create: pollOptions.map(option => { return { name: option } }) }, endDate: pollEndDate } } : null
     return ctx.db.mutation.createPost({
-      data: {
-        text,
-        images,
-        poll,
-        author: { connect: { id: userId },
-        },
-      },
+      data: { text, images, poll, author }
     }, info)
-    return null
   },
 
   async deletePost(parent, { id }, ctx, info) {
-    const userId = getUserId(ctx)
+    const author = { id: getUserId(ctx) }
     const postExists = await ctx.db.exists.Post({
       id,
-      author: { id: userId },
+      author
     })
     if (!postExists) {
       throw new Error(`Post not found or you're not the author`)
     }
-
     return ctx.db.mutation.deletePost({ where: { id } })
   },
 }
